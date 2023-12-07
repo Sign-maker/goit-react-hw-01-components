1888fa31f2a2de1a1be13376364ed76b866aeb6b
Sign-maker <muratovdv76@gmail.com>
2023-12-07 14:38:54 +0200

    json repaired

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
-import { user } from 'datajson/user';
+import user from 'datajson/user';
+import data from 'datajson/data.json';
+import friends from 'datajson/friends.json';
+import transactions from 'datajson/transactions.json';
 import { Profile } from './Profile/Profile';
 
 export const App = () => {
+  console.log(user);
   return (
     <>
       <Profile user={user}></Profile>
