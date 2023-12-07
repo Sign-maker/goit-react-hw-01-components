b42fcd26c67c387b240f2c005566dfbea2d2bbce
Sign-maker <muratovdv76@gmail.com>
2023-12-07 16:08:53 +0200

    Statistics done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,14 @@ import data from 'datajson/data.json';
 import friends from 'datajson/friends.json';
 import transactions from 'datajson/transactions.json';
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 
 export const App = () => {
-  console.log(user);
   return (
     <>
       <Profile user={user}></Profile>
+      <Statistics title="Upload stats" data={data}></Statistics>
+      <Statistics data={data}></Statistics>
     </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,25 @@
+import { getRandomHexColor } from 'utils/get-random-color';
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, data }) => {
+  return (
+    <section className={css.statistics}>
+      {/* {title.length > 0 && <h2 className={css.title}>{title}</h2>} */}
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statList}>
+        {data.map(({ id, label, percentage }) => {
+          return (
+            <li
+              key={id}
+              className={css.item}
+              style={{ backgroundColor: getRandomHexColor() }}
+            >
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{`${percentage}%`}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
